8ad5d60452669ac635f9447d17bf549de1f577cd
f2ire <yanisfiire@gmail.com>
2025-06-30 03:06:40 +0200

    ✨ New: Setup JWT authentication
    - Installed djangorestframework-simplejwt
    - Added /api/token/ and /api/token/refresh/ endpoints
    - Created ProtectedView with IsAuthenticated
    - Connected protected route at /api/protected/

--- focus/urls.py
+++ focus/urls.py
@@ -1,10 +1,11 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
-from .views import SessionViewSet
+from .views import SessionViewSet, ProtectedView
 
 router = DefaultRouter()
 router.register(r'sessions', SessionViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path("protected/", ProtectedView.as_view(), name = "protected"),
 ]
--- focus/views.py
+++ focus/views.py
@@ -6,3 +6,13 @@ from .serializers import SessionSerializer
 class SessionViewSet(viewsets.ModelViewSet):
     queryset = Session.objects.all()
     serializer_class = SessionSerializer
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+class ProtectedView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self,request):
+        return Response({"message": f"Hello {request.user.username}, you're authenticated!" })
